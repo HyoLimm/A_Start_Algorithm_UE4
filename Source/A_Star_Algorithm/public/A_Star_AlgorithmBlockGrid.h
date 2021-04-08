53852b633557efcd7b2c4905045b296801fac51a
EdwinH <37802083+HyoLimm@users.noreply.github.com>
2021-04-08 11:04:27 +0900

    Update A_Star_AlgorithmBlockGrid.h

--- Source/A_Star_Algorithm/public/A_Star_AlgorithmBlockGrid.h
+++ Source/A_Star_Algorithm/public/A_Star_AlgorithmBlockGrid.h
@@ -26,7 +26,7 @@ public:
 	AA_Star_AlgorithmPawn* PawnOwner;
 
 public:
-	/*루트 컴포넌트*/
+
 	UPROPERTY(Category = Grid, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USceneComponent* dummyRoot;
 
@@ -46,41 +46,41 @@ public:
 	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadWrite)
 	bool bDontCrossCorner;
 
-	//시작블록 지정
+
 	void SelectStartBlock(class AA_Star_AlgorithmBlock * StartBlock);
 
-	//목표블록 지정
+
 	void SelectTargetBlock(class AA_Star_AlgorithmBlock * TargetBlock);
 
-	//벽 블록 토글
+
 	void SetWallBlock(const FVector2D& targetPosition, const bool& IsWall=false);
 
-	/*선택 되어 있는 경로,시작,타겟 블록들)*/
+
 	void AllClearBlock();
 private:
-	/*초기화*/
+	
 	void SetRelease();
 
 	TArray<FVector2D> GetPath_While(const FVector2D& startPosition,const FVector2D& targetPosition);
 
-	/*목표지점 도착했는가?*/
+
 	bool GetArriveTarget(const FVector2D& startPosition, const FVector2D& targetPosition) const;
 
-	//배열 역순
+
 	void ReverseArray();
 
 	void  OpenListAdd(const int& currentX, const int& currentY);
 
 	bool CheckException(const FVector2D& targetPosition);
 
-	//경로 그리기
+
 	void DrawPath(const FVector2D& p_DrawPosition,const int& p_CurBlockNumber);
 
-	//블록 생성
+
 	void SpawnBlocks();
 
 private:
-	//경로로 지정 되어 있는 블록들 배열
+
 	TArray<AA_Star_AlgorithmBlock *> curPathBlocks;
 
 
@@ -88,25 +88,24 @@ private:
 
 	FVector2D mDirDiagnolInfo[4];
 
-	/*갈수 있는 주변길을 담아 놓는곳*/
+	
 	TArray<Node_Info*> mOpenList;
 
-	/*지나온 경로*/
+
 
 	TArray<Node_Info*> mClosedList;
 
 	Node_Info* mCurrentNode;
 
-	/*최종 경로*/
+
 	TArray<FVector2D> mFinalPathList;
 
-	/*캐릭터 경로*/
 	TArray<FVector2D> mCharacterPath;
 
 
-	/*전체 블록 관리*/
+
 	Node_Info mNodeArr[20][20];
-private://타이머 관련
+private:
 
 	void MovingTimer();
 
