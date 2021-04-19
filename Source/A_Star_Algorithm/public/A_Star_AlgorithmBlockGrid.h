14470944a0aa6d1419ae4bb0163b7d96f7b06f4d
EdwinH <37802083+HyoLimm@users.noreply.github.com>
2021-04-19 21:35:08 +0900

    Update A_Star_AlgorithmBlockGrid.h
    refactoring

--- Source/A_Star_Algorithm/public/A_Star_AlgorithmBlockGrid.h
+++ Source/A_Star_Algorithm/public/A_Star_AlgorithmBlockGrid.h
@@ -24,9 +24,7 @@ public:
 	AA_Star_AlgorithmBlock* curTargetBlock;
 
 	AA_Star_AlgorithmPawn* PawnOwner;
-
 public:
-
 	UPROPERTY(Category = Grid, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USceneComponent* dummyRoot;
 
@@ -46,26 +44,21 @@ public:
 	UPROPERTY(Category = Grid, EditAnywhere, BlueprintReadWrite)
 	bool bDontCrossCorner;
 
+public:
+	void SelectStartBlock(AA_Star_AlgorithmBlock * StartBlock);
 
-	void SelectStartBlock(class AA_Star_AlgorithmBlock * StartBlock);
-
-
-	void SelectTargetBlock(class AA_Star_AlgorithmBlock * TargetBlock);
-
+	void SelectTargetBlock(AA_Star_AlgorithmBlock * TargetBlock);
 
 	void SetWallBlock(const FVector2D& targetPosition, const bool& IsWall=false);
 
 
 	void AllClearBlock();
-private:
-	
+private:	
 	void SetRelease();
 
-	TArray<FVector2D> GetPath_While(const FVector2D& startPosition,const FVector2D& targetPosition);
-
+	TArray<FVector2D>& GetPath_While(const FVector2D& startPosition,const FVector2D& targetPosition);
 
 	bool GetArriveTarget(const FVector2D& startPosition, const FVector2D& targetPosition) const;
-
 
 	void ReverseArray();
 
@@ -73,40 +66,29 @@ private:
 
 	bool CheckException(const FVector2D& targetPosition);
 
-
 	void DrawPath(const FVector2D& p_DrawPosition,const int& p_CurBlockNumber);
 
-
 	void SpawnBlocks();
-
 private:
-
+	UPROPERTY()
 	TArray<AA_Star_AlgorithmBlock *> curPathBlocks;
-
 
 	FVector2D mDirInfo[4];
 
 	FVector2D mDirDiagnolInfo[4];
 
-	
 	TArray<Node_Info*> mOpenList;
-
-
 
 	TArray<Node_Info*> mClosedList;
 
 	Node_Info* mCurrentNode;
 
-
 	TArray<FVector2D> mFinalPathList;
 
 	TArray<FVector2D> mCharacterPath;
 
-
-
 	Node_Info mNodeArr[20][20];
 private:
-
 	void MovingTimer();
 
 	int32 mCurPathCountNum;
