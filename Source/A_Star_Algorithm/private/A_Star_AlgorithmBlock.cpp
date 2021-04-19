224e1b395412a69b9688200cb936e8b227eada91
EdwinH <37802083+HyoLimm@users.noreply.github.com>
2021-04-19 22:29:16 +0900

    Update A_Star_AlgorithmBlock.cpp

--- Source/A_Star_Algorithm/private/A_Star_AlgorithmBlock.cpp
+++ Source/A_Star_Algorithm/private/A_Star_AlgorithmBlock.cpp
@@ -22,7 +22,7 @@ AA_Star_AlgorithmBlock::AA_Star_AlgorithmBlock()
 		ConstructorHelpers::FObjectFinderOptional<UMaterial> BaseMaterial;
 		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> BlueMaterial;
 		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> OrangeMaterial;
-		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> PinkMaterial; //추가
+		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> PinkMaterial; //異붽?
 		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> RedMaterial;
 		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> PurpleMaterial;
 		ConstructorHelpers::FObjectFinderOptional<UMaterialInstance> BlackMaterial;
@@ -32,7 +32,7 @@ AA_Star_AlgorithmBlock::AA_Star_AlgorithmBlock()
 			, BaseMaterial(TEXT("/Game/Puzzle/Meshes/BaseMaterial.BaseMaterial"))
 			, BlueMaterial(TEXT("/Game/Puzzle/Meshes/BlueMaterial.BlueMaterial"))
 			, OrangeMaterial(TEXT("/Game/Puzzle/Meshes/OrangeMaterial.OrangeMaterial"))
-			, PinkMaterial(TEXT("/Game/Puzzle/Meshes/PinkMaterial.PinkMaterial")) //추가
+			, PinkMaterial(TEXT("/Game/Puzzle/Meshes/PinkMaterial.PinkMaterial")) //異붽?
 			, RedMaterial(TEXT("/Game/Puzzle/Meshes/RedMaterial.RedMaterial"))
 			, PurpleMaterial(TEXT("/Game/Puzzle/Meshes/PurpleMaterial.PurpleMaterial"))
 			, BlackMaterial(TEXT("/Game/Puzzle/Meshes/BlackMaterial.BlackMaterial"))
@@ -52,17 +52,17 @@ AA_Star_AlgorithmBlock::AA_Star_AlgorithmBlock()
 	mBlockMesh->SetMaterial(0, ConstructorStatics.BlueMaterial.Get());
 	mBlockMesh->SetupAttachment(mDummyRoot);
 
-	//추가
+	//異붽?
 	mBlockText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TEXT"));
 	mBlockText->SetupAttachment(mDummyRoot);
 
-	//TEXT 위치조절
+	//TEXT ?꾩튂議곗젅
 	mBlockText->SetRelativeLocation(FVector(-60.f, -100.f, 60.0f));
 	mBlockText->SetRelativeRotation(FRotator(90.f, 180.f, 0.f));
 	mBlockText->SetXScale(5.f);
 	mBlockText->SetYScale(5.f);
 
-	//색상,초반 숨김
+	//?됱긽,珥덈컲 ?④?
 	mBlockText->SetTextRenderColor(FColor::Black);
 	mBlockText->SetVisibility(false);
 
@@ -72,7 +72,7 @@ AA_Star_AlgorithmBlock::AA_Star_AlgorithmBlock()
 	mOrangeMaterial = ConstructorStatics.OrangeMaterial.Get();
 	mPurpleMaterial = ConstructorStatics.PurpleMaterial.Get();
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//추가
+	//異붽?
 	mPinkMaterial = ConstructorStatics.PinkMaterial.Get();
 	mRedMaterial = ConstructorStatics.RedMaterial.Get();
 	mBlackMaterial = ConstructorStatics.BlackMaterial.Get();
@@ -84,15 +84,15 @@ void AA_Star_AlgorithmBlock::HandleClicked() {
 	if (mOwningGridRef == nullptr) return;
 
 	if (!bIsClicked) {
-		if (mOwningGridRef->curStartBlock == nullptr) { //시작 지점 지정
-			mBlockMesh->SetMaterial(0, mOrangeMaterial); //시작 블럭 색상 변경
-			mOwningGridRef->SelectStartBlock(this);//시작 블럭 지정			
+		if (mOwningGridRef->GetCurStartBlock() == nullptr) { //?쒖옉 吏??吏??
+			mBlockMesh->SetMaterial(0, mOrangeMaterial); //?쒖옉 釉붾윮 ?됱긽 蹂寃?
+			mOwningGridRef->SelectStartBlock(this);//?쒖옉 釉붾윮 吏??		
 			bIsClicked = true;
 
 		}
-		else if (mOwningGridRef->curTargetBlock == nullptr) { //목표지정 지정
-			mBlockMesh->SetMaterial(0, mRedMaterial); //목표지점 색상 변경
-			mOwningGridRef->SelectTargetBlock(this); //목표 블록 지정
+		else if (mOwningGridRef->GetCurTargetBlock() == nullptr) { //紐⑺몴吏??吏??
+			mBlockMesh->SetMaterial(0, mRedMaterial); //紐⑺몴吏???됱긽 蹂寃?
+			mOwningGridRef->SelectTargetBlock(this); //紐⑺몴 釉붾줉 吏??
 			bIsClicked = true;
 		}
 	}
@@ -104,19 +104,19 @@ void AA_Star_AlgorithmBlock::Highlight(bool p_bOnOverlap) {
 	if (mOwningGridRef == nullptr)
 		return;
 
-	// 하이라이트 효과 전 검사 (이미 눌러져있는제/경로블록/벽 여부)
+	// ?섏씠?쇱씠???④낵 ??寃??(?대? ?뚮윭?몄엳?붿젣/寃쎈줈釉붾줉/踰??щ?)
 	if (bIsClicked || bIsPath || bIsWall)
 		return;
 
 
-	//마우스 커서가 오버랩 되었을때 색상 변경
+	//留덉슦??而ㅼ꽌媛 ?ㅻ쾭???섏뿀?꾨븣 ?됱긽 蹂寃?
 	if (p_bOnOverlap) 
 	{
-		if (mOwningGridRef->curStartBlock == nullptr && mOwningGridRef->curTargetBlock == nullptr) 
+		if (mOwningGridRef->GetCurStartBlock() == nullptr && mOwningGridRef->GetCurTargetBlock() == nullptr) 
 		{
 			mBlockMesh->SetMaterial(0, MouseOverlapMaterial);
 		}
-		else if (mOwningGridRef->curTargetBlock == nullptr) 
+		else if (mOwningGridRef->GetCurTargetBlock() == nullptr) 
 		{
 			mBlockMesh->SetMaterial(0, mPinkMaterial);
 		}
@@ -127,16 +127,16 @@ void AA_Star_AlgorithmBlock::Highlight(bool p_bOnOverlap) {
 	}
 }
 //////////////////Setter//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*기본 상태 색상변경*/
+//湲곕낯 ?곹깭 ?됱긽蹂寃?/
 void AA_Star_AlgorithmBlock::SetBasicMaterial()
 {
 	mBlockMesh->SetMaterial(0, mBaseMaterial);
 }
 
-//길 블록 여부
+//湲?釉붾줉 ?щ?
 void AA_Star_AlgorithmBlock::SetPathBlock() 
 {
-	//IsPath(현재상태)검사하여 경로로 지정되어 있지 않으면 경로 블록으로 설정
+	//IsPath(?꾩옱?곹깭)寃?ы븯??寃쎈줈濡?吏?뺣릺???덉? ?딆쑝硫?寃쎈줈 釉붾줉?쇰줈 ?ㅼ젙
 	if (bIsPath == false) 
 	{
 		bIsPath = true;
@@ -157,9 +157,9 @@ void AA_Star_AlgorithmBlock::SetSearchBlock()
 	mBlockMesh->SetMaterial(0, mSearchMaterial);
 }
 
-//벽 여부 변경
+//踰??щ? 蹂寃?
 void AA_Star_AlgorithmBlock::SetWallBlock(const bool& IsWall) {
-	//이미 클릭되어 있거나, 경로로 되어있다면 벽으로 설정 불가능
+	//?대? ?대┃?섏뼱 ?덇굅?? 寃쎈줈濡??섏뼱?덈떎硫?踰쎌쑝濡??ㅼ젙 遺덇???
 	if (bIsClicked || bIsPath)
 	{
 		return;
